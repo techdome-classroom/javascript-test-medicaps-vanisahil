5ad17dacc3a456724748623b2a28cee80e370b6d
sahil vani <svani045@gmail.com>
2024-04-25 13:37:33 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,7 +1,7 @@
 function longestSubstring(s) {
     let longest = 0; 
-    let start = 0; // Starting index of the current substring
-    const charIndexMap = new Map(); // Map to store the index of each character
+    let start = 0;
+    const charIndexMap = new Map(); 
     
     // Loop through each character in the string
     for (let end = 0; end < s.length; end++) {
