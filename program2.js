2f586597e3d4cd8ef2a687540c9e4c2baea7a668
sahil vani <svani045@gmail.com>
2024-04-25 13:43:55 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,23 @@
 function smallestMissingPositiveInteger(nums) {
+// Remove non-positive integers
+nums = nums.filter(num => num > 0);
 
-  // Implement the function smallest_missing_positive_integer
+// Sort the array
+nums.sort((a, b) => a - b);
 
+// Initialize smallest positive integer
+let smallest = 1;
+
+// Find the smallest missing positive integer
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] === smallest) {
+    smallest++;
+  } else if (nums[i] > smallest) {
+    break;
+  }
+}
+
+return smallest;
 }
 
 module.exports = smallestMissingPositiveInteger;
